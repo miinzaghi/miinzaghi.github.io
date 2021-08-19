6f08577cb8f64be30ecd457d8e48c04b53ee2123
miinzaghi <88478797+miinzaghi@users.noreply.github.com>
2021-08-19 16:27:24 +0900

    Delete project directory

--- project/parachutes/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-                $("#contents").click(function(){
-                    $("#popup").fadeIn();
-                });
-                $("#popup").click(function(){
-                    $("#popup").fadeOut();
-                });
-});
